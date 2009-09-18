0b877dbc3597c9aabc2a83a0a93e552f32d0ed5c
mdonohue <mdonohue@dev.java.net>
2009-09-18 04:56:39 +0000

    Fix issue 4384

--- src/main/java/join/JoinTrigger.java
+++ src/main/java/join/JoinTrigger.java
@@ -162,7 +162,8 @@ public class JoinTrigger extends Recorder {
             if (parameterizedTrigger != null) {
                 list.add(Hudson.getInstance().getDescriptorByType(hudson.plugins.parameterizedtrigger.BuildTrigger.DescriptorImpl.class));
             }
-            list.add(Hudson.getInstance().getDescriptorByType(hudson.tasks.Mailer.DescriptorImpl.class));
+            // See issue 4384.  Supporting the mailer here breaks its use as the regular post-build action.
+            //list.add(Hudson.getInstance().getDescriptorByType(hudson.tasks.Mailer.DescriptorImpl.class));
             return list;
         }
         
