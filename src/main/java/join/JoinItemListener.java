71f63b7b9dad86196df1b97d704a61e359568eb7
Nicolas De Loof <nicolas.deloof@gmail.com>
2012-07-06 13:29:59 +0200

    update join job configuration on item rename with support for hierarchical job model

---
+++ src/main/java/join/JoinItemListener.java
@@ -0,0 +1,41 @@
+package join;
+
+import hudson.Extension;
+import hudson.model.Item;
+import hudson.model.Project;
+import hudson.model.listeners.ItemListener;
+import jenkins.model.Jenkins;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * @author: <a hef="mailto:nicolas.deloof@gmail.com">Nicolas De Loof</a>
+ */
+@Extension
+public class JoinItemListener extends ItemListener {
+
+    @Override
+    public void onRenamed(Item item, String oldName, String newName) {
+
+        String context =  item.getParent().getFullName();
+        if (context.length() > 0) context = context + "/";
+
+        for( Project<?,?> p : Jenkins.getInstance().getAllItems(Project.class) ) {
+            JoinTrigger t = p.getPublishersList().get(JoinTrigger.class);
+            if(t!=null) {
+                if(t.onJobRenamed(p.getParent(), Items.getCanonicalName(item.getParent(), oldName),
+                                                 Items.getCanonicalName(item.getParent(), newName))) {
+                    try {
+                        p.save();
+                    } catch (IOException e) {
+                        LOGGER.log(Level.WARNING, "Failed to persist project setting during rename from "+oldName+" to "+newName,e);
+                    }
+                }
+            }
+        }
+    }
+
+    private static final Logger LOGGER = Logger.getLogger(JoinItemListener.class.getName());
+}
